cdcc894f248b6e1c26c46c5c7c12ef74cbfd15cb
Gabriel Chartier <gabriel@chartier.link>
2025-08-07 11:26:18 -0500

    add html-day-austin-2025 page

---
+++ src/routes/html-day-austin-2025/+page.svelte
@@ -0,0 +1,569 @@
+<svelte:head>
+	<title>HTML Day 2025 ✳️ Austin, TX</title>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✳️</text></svg>"
+	/>
+</svelte:head>
+
+<aside class="aside-disclaimer">
+	<p>
+		This page was made for HTML Day Austin 2025, a local event inspired by
+		<a href="https://html.energy" target="_blank">HTML Energy</a>. Originally hosted on
+		<a href="https://htmlaustin.com" target="_blank">htmlaustin.com</a>.
+	</p>
+</aside>
+
+<div class="container">
+	<header>
+		<div class="header-content">
+			<div class="title-container">
+				<img
+					src="html-day-austin-2025/html.gif"
+					alt="An animated gif of an HTML coin flipping."
+					class="title-html-gif"
+				/>
+				<h1 class="main-title">
+					<a href="https://html.energy/html-day/2025/index.html" target="_blank">HTML Day 2025</a>
+					✳️ Austin, TX!
+				</h1>
+				<img
+					src="html-day-austin-2025/texas.gif"
+					alt="An animated gif of a Texas flag waving in the wind."
+					class="title-texas-gif"
+				/>
+			</div>
+			<img
+				src="html-day-austin-2025/html_man.png"
+				alt="A dithered pic of the silhouette of a human gently levitating above the ground, their heart chakra glowing green, set against a background of a large glowing HTML Energy orb of light."
+				class="hero-image"
+			/>
+		</div>
+	</header>
+
+	<div class="rsvp-widget">
+		<p class="rsvp-title">RSVP:</p>
+		<a href="https://lu.ma/a9zk3mjw" target="_blank">
+			<img
+				src="html-day-austin-2025/click.gif"
+				alt="A button that has the text 'Click here' spinning around it."
+				class="rsvp-button"
+			/>
+		</a>
+	</div>
+
+	<main>
+		<details class="details-what-is-this" open>
+			<summary>What is this?</summary>
+
+			<p>
+				<a href="https://html.energy/html-day/2025/index.html" target="_blank">HTML Day</a>
+				is a <strong>FREE</strong>, beginner‑friendly, all-ages event where people from all over the
+				world meet in-person to learn about and write their own handmade HTML webpages!
+			</p>
+			<p>
+				<img
+					src="html-day-austin-2025/dance.gif"
+					alt="An animated gif of a person dancing ecstatically with flashing text that says 'HTML'"
+					class="dance-gif"
+				/>
+				<a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">HTML</a>
+				is the most basic building block of the Web. It forms the structure and meaning of every website
+				you've ever visited.
+			</p>
+			<p>
+				<a href="https://html.energy/index.html" target="_blank">HTML Energy</a> is all around us and
+				in this very website. It's worth celebrating!
+			</p>
+		</details>
+		<details>
+			<summary>Who is it for?</summary>
+			<img
+				src="html-day-austin-2025/you.gif"
+				class="you-gif"
+				alt="An animated gif of the word 'you' flashing in different colors and fonts."
+			/>
+
+			<p>Literally anyone. No coding skills or prior experience required.</p>
+			<p>
+				Everyone can learn to code in HTML. If you can read and write (or type) then you can write
+				HTML!
+			</p>
+			<p>Don't worry if you don't know HTML, we'll all help each other out. 😃</p>
+		</details>
+		<details>
+			<summary>Where and when?</summary>
+			<ul>
+				<li>
+					<p><b>🗓️ Date:</b> Saturday, August 2nd</p>
+				</li>
+				<li>
+					<p><b>🕰️ Time:</b> 1:30pm-4:30pm CDT</p>
+				</li>
+				<li>
+					<p>
+						<b>🗺️ Location:</b> Austin, TX. Exact location TBD. <br /><br />
+						It will very likely be at one of the Austin Public Library branches.
+						<br /><br />
+						Exact location will be updated on the
+						<a href="https://lu.ma/a9zk3mjw" target="_blank">Luma event page</a>
+						and the
+						<a href="https://www.facebook.com/events/1044166324371710" target="_blank"
+							>Facebook event page</a
+						>. If you've RSVPed, you'll get an email with the exact location.
+					</p>
+				</li>
+			</ul>
+		</details>
+		<details>
+			<summary>Who is hosting / organizing this?</summary>
+			<p>
+				💠 Gabriel Chartier -
+				<a href="https://www.gabriel-export.earth" target="_blank">www.gabriel-export.earth</a>
+			</p>
+			<p>
+				If you need to contact me for any reason, please email me at
+				<a href="mailto:gchartier@pm.me" target="_blank">gchartier@pm.me</a>
+			</p>
+		</details>
+		<details>
+			<summary>What should I expect?</summary>
+			<div class="expectations-grid">
+				<img
+					src="html-day-austin-2025/help.gif"
+					alt="An animated gif of a computer with a smiley face on it and text that says: 'Help everyone learn the net'"
+				/>
+				<img
+					src="html-day-austin-2025/interactive.gif"
+					alt="An animated gif of some flashing green text that says 'interactive'"
+				/>
+				<img
+					src="html-day-austin-2025/free.gif"
+					alt="An animated gif of a dollar sign with a red cross through it spinning on top of a yellow block with text on it that says 'FREE'"
+				/>
+			</div>
+
+			<p>
+				Expect to have fun, meet some new people, ask for help, help others, and most importantly
+				write your own HTML website! We'll be writing and coding in HTML and CSS primarily, but you
+				may use JS if you want to.
+			</p>
+			<br />
+			<div>
+				<table class="schedule-table">
+					<caption class="schedule-caption"> Here's a tentative schedule: </caption>
+					<thead>
+						<tr>
+							<th scope="col">Time</th>
+							<th scope="col">Activity</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1:30pm</th>
+							<td>Welcome (about event, HTML basics, etc.)</td>
+						</tr>
+						<tr>
+							<th scope="row">2:00pm</th>
+							<td>Freely writing HTML time (we'll have some prompts)</td>
+						</tr>
+						<tr>
+							<th scope="row">4:00pm</th>
+							<td>Shareout (for those comfortable)</td>
+						</tr>
+						<tr>
+							<th scope="row">4:30pm</th>
+							<td>End of event</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</details>
+		<details>
+			<summary>What should I bring?</summary>
+			<ul>
+				<li>
+					<strong>Essentials:</strong>
+					<ul>
+						<li>🧍You</li>
+						<li>
+							✍️ Something to write HTML on:
+							<ul>
+								<li>💻 Fully charged laptop (recommended)</li>
+								<li>📱 Phone or Tablet</li>
+								<li>📝 Pen & paper</li>
+								<li>🗿 Stone tablet & chisel</li>
+							</ul>
+						</li>
+						<li>🔋 Chargers, batteries, etc.</li>
+						<li>🚰 Water</li>
+					</ul>
+				</li>
+				<li>
+					<strong>Optionals:</strong>
+					<ul>
+						<li>👫 A friend, or two</li>
+						<li>💾 USB drive</li>
+						<li>🍎 Snacks to share (if you want)</li>
+					</ul>
+				</li>
+			</ul>
+		</details>
+		<details>
+			<summary>I'm in! What now?</summary>
+			<ul>
+				<li>
+					<h3>RSVP (optional but recommended)</h3>
+					<p>
+						RSVPing is not required, but it's helpful for gauging attendance, and sending event
+						reminders / updates in case anything changes.
+					</p>
+					<p>
+						You can RSVP via the
+						<a href="https://lu.ma/a9zk3mjw" target="_blank">Luma event page</a> or the
+						<a href="https://www.facebook.com/events/1044166324371710" target="_blank"
+							>Facebook event page</a
+						>.
+					</p>
+					<p>
+						By RSVPing, you'll get a limited edition handmade HTML Day Austin 2025 Zine! (We'll make
+						a few extras for those who do not RSVP.)
+					</p>
+				</li>
+			</ul>
+		</details>
+		<details>
+			<summary>What happens after the event?</summary>
+			<p>
+				We'll take our energy with us and keep it alive.
+				<img
+					src="html-day-austin-2025/sun.gif"
+					alt="An animated gif of a sun shining brightly."
+					class="sun-gif"
+				/>
+			</p>
+			<p>
+				Photos taken during the event can be found on
+				<a href="https://www.are.na/html-energy/html-day-2025-photo-album" target="_blank">Arena</a
+				>.
+			</p>
+		</details>
+		<details>
+			<summary>Not in Austin but interested?</summary>
+			<p>
+				Check out HTML Energy's
+				<a href="https://html.energy/events.html" target="_blank">events page</a>. If your city
+				isn't listed, feel free to organize your own!
+			</p>
+		</details>
+		<details>
+			<summary>HTML Resources</summary>
+			<p>If you want to brush up on HTML before the event, here's some resources:</p>
+			<ul>
+				<li>
+					<a
+						href="https://docs.google.com/presentation/d/1KDDbJaRVJe2kQQ2U_Wwdq7RobrsrbN2UeX9xqKrdLyo/edit?slide=id.gaa3444400b_0_101#slide=id.gaa3444400b_0_101"
+						target="_blank">HTML Basics (Google Slides)</a
+					>
+				</li>
+				<li>
+					<a href="https://htmlforpeople.com/intro/" target="_blank">HTML for People</a>
+				</li>
+				<li>
+					<a
+						href="https://docs.google.com/document/d/1Keydym-tNjlGGkaCx_c2P9kr6hr8-bS_HGTjbA96XYs/edit?pli=1&tab=t.0#heading=h.o6o2d0bmrzvi"
+						target="_blank">HTML Cheatsheet</a
+					>
+				</li>
+				<li>
+					<a href="https://uploads.html.green/files/zine-2024-08.pdf" target="_blank"
+						>HTML Cookbook Zine</a
+					>
+				</li>
+				<li>
+					<a href="https://ritualdust.com/craft/make-yourself-a-website/" target="_blank"
+						>Make Yourself a Website</a
+					>
+				</li>
+				<li>
+					<a
+						href="https://developer.mozilla.org/en-US/docs/Learn_web_development/Getting_started/Your_first_website/What_will_your_website_look_like"
+						target="_blank">Your First Website (MDN)</a
+					>
+				</li>
+				<li>
+					<a href="https://coolguy.website/web-zine/01/" target="_blank">Web Zine 01</a>
+				</li>
+				<li>
+					<a href="https://htmlreference.io/" target="_blank">HTML Reference</a>
+				</li>
+				<li>
+					<a href="https://cssreference.io/" target="_blank">CSS Reference</a>
+				</li>
+				<li>
+					<a href="https://joeldare.com/why-im-writing-pure-html-and-css-in-2025" target="_blank"
+						>Why I'm writing pure HTML and CSS in 2025</a
+					>
+				</li>
+			</ul>
+		</details>
+	</main>
+	<footer>
+		<p>✳️ Written in pure, raw HTML. Can you feel the energy?</p>
+		<p>🔎 Want to see? View the source!</p>
+		<p>
+			🏛️ All gifs are from
+			<a href="https://gifcities.org/search?q=html&offset=0&page_size=200" target="_blank"
+				>Gifcities</a
+			>
+		</p>
+	</footer>
+</div>
+
+<style>
+	.aside-disclaimer {
+		border-bottom: 4px solid black;
+		background-color: #70f206;
+		padding: 0.4rem;
+		color: black;
+		margin: 0;
+		width: 100%;
+		text-align: center;
+		font-size: 1rem;
+		font-family: serif;
+
+		p {
+			margin: 0;
+		}
+	}
+
+	.container {
+		height: 100%;
+		min-height: 100%;
+		min-width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		margin: 0;
+		padding: 0 8px 8px 8px;
+		font-family: serif;
+	}
+
+	header {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		padding-top: 1rem;
+
+		.header-content {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+
+		.title-container {
+			display: flex;
+			flex-wrap: wrap;
+			gap: 0.2rem;
+			padding: 1rem 0;
+		}
+
+		.main-title {
+			font-size: 3rem;
+			margin: 0 0 0 1rem;
+			line-height: 1.2;
+			font-weight: bold;
+		}
+
+		.hero-image {
+			width: 20rem;
+			height: auto;
+			display: block;
+			border: 1rem ridge rgb(211 220 50 / 0.6);
+			margin: 2rem 0;
+		}
+
+		.title-html-gif {
+			margin-bottom: -1rem;
+			width: 5rem;
+			height: 3.5rem;
+		}
+
+		.title-texas-gif {
+			margin-left: 1rem;
+			margin-top: -0.2rem;
+			width: 5rem;
+			height: 4rem;
+		}
+	}
+
+	.rsvp-widget {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		border: 5px dotted black;
+		position: fixed;
+		right: 0.5rem;
+		top: 17rem;
+		background: white;
+		padding: 1rem;
+		z-index: 1000;
+
+		.rsvp-title {
+			margin: 0;
+			padding: 0.2rem;
+			margin-top: 0.2rem;
+			font-weight: bold;
+			color: #70f206;
+			background-color: black;
+			font-size: 1.5rem;
+		}
+
+		.rsvp-button {
+			width: 100%;
+			height: auto;
+		}
+	}
+
+	main {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+		max-width: 800px;
+		padding: 1rem;
+		gap: 1rem;
+	}
+
+	details {
+		border: 2px solid black;
+		border-radius: 3px;
+		position: relative;
+		font-size: 1.2rem;
+		width: 100%;
+
+		p,
+		div {
+			padding: 0.5rem;
+		}
+	}
+
+	.details-what-is-this {
+		position: relative;
+	}
+
+	summary {
+		color: black;
+		font-size: 2rem;
+		padding: 0;
+		width: 100%;
+		cursor: cell;
+		line-height: 1.2;
+	}
+
+	summary + * {
+		margin-top: 0.8rem;
+	}
+
+	details > :last-child {
+		margin-bottom: 0.8rem;
+	}
+
+	summary:hover,
+	details[open] summary {
+		background-color: black;
+		color: #70e106;
+	}
+
+	.dance-gif {
+		width: 6rem;
+		height: auto;
+		float: right;
+		margin-right: 0.5rem;
+	}
+
+	.you-gif {
+		height: 4rem;
+		width: auto;
+		padding: 1rem;
+		padding-bottom: 0;
+	}
+
+	.sun-gif {
+		margin-bottom: -0.1rem;
+		display: inline;
+	}
+
+	.expectations-grid {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 2rem;
+		margin-top: 1rem;
+
+		img {
+			height: 3rem;
+			width: auto;
+		}
+
+		img:last-child {
+			height: 5rem;
+		}
+	}
+
+	.schedule-table {
+		table-layout: fixed;
+		width: 100%;
+		margin: -2rem auto 1rem auto;
+		border-collapse: collapse;
+		border-top: 1px solid #999;
+		border-bottom: 1px solid #999;
+
+		.schedule-caption {
+			padding: 1em;
+			padding-left: 0.2rem;
+			font-style: italic;
+			font-weight: bold;
+			font-size: 1.2rem;
+			caption-side: top;
+			letter-spacing: 1px;
+			text-align: left;
+		}
+
+		tbody tr:nth-child(odd) {
+			background-color: #eee;
+		}
+
+		th,
+		td {
+			vertical-align: top;
+			padding: 0.3em;
+		}
+
+		tr :nth-child(2) {
+			text-align: left;
+			width: 75%;
+		}
+
+		tr :nth-child(1) {
+			text-align: left;
+			width: 25%;
+		}
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		align-items: center;
+		margin: 0 0 2rem 0;
+		gap: 0.5rem;
+		font-size: 1.1rem;
+
+		p {
+			margin: 0;
+		}
+	}
+</style>
