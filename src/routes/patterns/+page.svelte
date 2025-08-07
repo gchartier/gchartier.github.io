853fabaa74fa12f97f4e885a91c1843f6e57841d
Gabriel Chartier <gabriel@chartier.link>
2025-08-07 09:08:30 -0500

    add patterns page made on HTML Day Austin 2025

---
+++ src/routes/patterns/+page.svelte
@@ -0,0 +1,89 @@
+<aside class="border-b-4 border-black bg-lime-400 p-2 text-black">
+	<p>
+		This page was made during <a
+			href="https://www.gabriel-export.earth/html-day-austin-2025"
+			target="_blank">HTML Day Austin 2025</a
+		>, a local event inspired by <a href="https://html.energy" target="_blank">HTML Energy</a>
+	</p>
+</aside>
+<main>
+	<div
+		style="width: fit-content; background: lightgray; border-radius: 20px; padding: 2rem; border: 5px solid black;"
+	>
+		<h1>What is a Pattern?</h1>
+		<p>There's actually a lot of different kinds out there...</p>
+
+		<h2>Textiles, Fabrics, and Tiles</h2>
+		<div style="display: flex; gap: 4rem;">
+			<div>
+				<p>Maybe you think of plaid?</p>
+				<img
+					src="plaid.gif"
+					style="width: 10rem;"
+					alt="a dog in a picnic basekt with a plaid blanket"
+				/>
+			</div>
+
+			<div>
+				<p>or houndstooth?</p>
+				<img src="houndstooth.gif" style="width: 10rem;" alt="houndstooth pattern" />
+			</div>
+
+			<div>
+				<p>or some bathroom tiles?</p>
+				<img src="tiles.jpg" style="width: 10rem;" alt="houndstooth pattern" />
+			</div>
+		</div>
+
+		<h2>Nature</h2>
+		<p style="padding-bottom: 1rem;">Maybe you might think of patterns in nature.</p>
+		<div style="display: flex; gap: 4rem;">
+			<div>
+				<p>Like the ripples in dunes...</p>
+				<img src="dunes.jpg" alt="sand dunes with ripples in the sand" style="width: 20rem;" />
+			</div>
+
+			<div>
+				<p>or the waves of the sea</p>
+				<img src="waves.gif" alt="a gif of waves in the sea" style="width: 20rem; height: 3rem;" />
+				<p>
+					or the spirals in shells <img
+						src="shell.webp"
+						alt="a nautilus shell with the golden ratio spiral"
+						style="width: 20rem;"
+					/>
+				</p>
+			</div>
+
+			<div>
+				<p>
+					or how lightning, tree roots, tree branches, and human veins all look the same. <span
+						style="background-color: yellow;">wtf is up with that</span
+					>
+				</p>
+				<img
+					src="fractals.jpg"
+					alt="examples of fractals in nature looking the same like tree roots, tree branches, rivers, lightning, and leaf veins"
+					style="width: 20rem;"
+				/>
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		padding: 3rem;
+		background-image: url('bg.gif');
+	}
+
+	h1 {
+		font-size: 3rem;
+		font-weight: bold;
+	}
+
+	h2 {
+		font-size: 2rem;
+		margin-top: 2rem;
+	}
+</style>
