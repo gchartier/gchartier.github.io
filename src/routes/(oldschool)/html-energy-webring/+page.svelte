23a42b32e9697c0423c431095a4cf780b46ac89b
Gabriel Chartier <gabriel@chartier.link>
2026-02-12 12:45:46 -0600

    update webring member's site

--- src/routes/(oldschool)/html-energy-webring/+page.svelte
+++ src/routes/(oldschool)/html-energy-webring/+page.svelte
@@ -26,7 +26,7 @@
 		'https://elijahposttrash.neocities.org/',
 		'https://sonechka.bouvardia.blue/',
 		'https://cyanidefish.neocities.org/',
-		'https://raster.foo.ng',
+		'https://r.foo.ng',
 		'https://xyzzyzzyzx.xyz/home.html',
 		'https://devhank.neocities.org/sakuramiku/sakuramiku',
 		'https://sumthing.neocities.org'
